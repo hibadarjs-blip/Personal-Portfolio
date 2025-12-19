658c6359afbbd3a0e1efd1bb03d115bfa31f4739
Hiba Dar <hibadar.js@gmail.com>
2025-12-19 23:44:28 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+const menuBtn = document.querySelector("#menuBtn");
+const openIcon = document.querySelector("#openIcon");
+const closeIcon = document.querySelector("#closeIcon");
+const mobileNav = document.querySelector("#mobileNav");
+const mobileLinks = mobileNav.querySelectorAll("a");
+const scrollTopBtn = document.querySelector("#scrollTopBtn");
+function toggleMenu(){
+    const isExpanded = menuBtn.getAttribute("aria-expanded") === 'true';
+
+    menuBtn.setAttribute("aria-expanded", !isExpanded)
+
+    mobileNav.setAttribute("aria-hidden", isExpanded);
+
+    if (!isExpanded) {
+    closeIcon.style.display = "block"; 
+    openIcon.style.display = "none";  
+    mobileNav.classList.add("open");         
+    }else{
+    closeIcon.style.display = "none"; 
+    openIcon.style.display = "block";  
+    mobileNav.classList.remove("open");      
+    }
+}
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      scrollTopBtn.style.display = "block";
+    } else {
+      scrollTopBtn.style.display = "none";
+    }
+});
+
+scrollTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+});
+
+menuBtn.addEventListener("click", ()=>{
+    toggleMenu();
+});
+
+mobileLinks.forEach((link) =>{
+    link.addEventListener("click", ()=>{
+        if (mobileNav.classList.contains("open")) {
+            toggleMenu();
+        }     
+});
+})
+
